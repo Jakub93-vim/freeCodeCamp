cf4b92cfc4eaef2788895ef65270fcc086f95fe3
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2020-11-30 20:07:27 +0100

    Update main.py
    Pridani podtrzeni, odstraneni prvni sekce bez smycky

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 def arithmetic_arranger(problems):
     import re
-    '''
-    prvni = re.search("\d*\s", problems[0]).group()
-    znamenko = re.search("[-|+]", problems[0]).group()
-    druhy = re.search("\s\d+", problems[0]).group()
-    arranged_problems= print(prvni + '\n' + znamenko + druhy + '\n' + '----')
-    '''
+
     prvniradek = ''
     druhyradek = ''
     znamenko = ''
-    for i in range (len(problems)):
+    podtrzeni=''
+    delka=(len(problems))
+    for i in range (delka):
+        de1 = 6 - len(re.search("\d*\s", problems[i]).group())
+        de2 = 4 - len(re.search("\s\d+", problems[i]).group())
+        #print(de1,de2)
         znamenko = re.search("[-|+]", problems[i]).group()
-        prvniradek=prvniradek + (re.search("\d*\s", problems[i]).group())
-        druhyradek= druhyradek + znamenko + (re.search("\s\d+", problems[i]).group())
-    print(prvniradek + '\n' + druhyradek)
+        prvniradek=prvniradek + de1*" " + 3*" " + (re.search("\d*\s", problems[i]).group())
+        druhyradek= druhyradek + de2*" " + 3*" " + znamenko +  (re.search("\s\d+", problems[i]).group())
+        podtrzeni = podtrzeni + 1*" " +max(de1,de2)*' ' + (max(de1,de2)+2)*'-'
+    print(prvniradek + '\n' + druhyradek + '\n' + podtrzeni)
     #return arranged_problems,rada
-
-
 
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
